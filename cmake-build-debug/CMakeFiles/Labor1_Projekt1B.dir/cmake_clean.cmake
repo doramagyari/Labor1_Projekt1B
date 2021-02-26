f778f2b06678efd885e0af2920f3411c7acd8ded
doramagyari <magyari.dora@student.ms.sapientia.ro>
2021-02-26 09:42:56 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Labor1_Projekt1B.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Labor1_Projekt1B.dir/main.c.obj"
+  "Labor1_Projekt1B.exe"
+  "Labor1_Projekt1B.exe.manifest"
+  "Labor1_Projekt1B.pdb"
+  "libLabor1_Projekt1B.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Labor1_Projekt1B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
